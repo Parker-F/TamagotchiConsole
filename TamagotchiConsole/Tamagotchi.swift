3de4077c519f5edb4ef0ac6df93d2955aed65aeb
Fred Parker <parker.f@etoncollege.org.uk>
2023-11-21 11:08:47 +0000

    Initial commit of Tamagotchi

---
+++ TamagotchiConsole/Tamagotchi.swift
@@ -0,0 +1,49 @@
+//
+//  Tamagotchi.swift
+//  TamagotchiConsole
+//
+//  Created by Parker, Fred (NPTL) on 21/11/2023.
+//
+
+import Foundation
+class Tamagotchi {
+    var weight : Int
+    var hungerHearts : Float
+    var happyHearts : Float
+    let name: String
+    var age: Int
+    var stage: String
+    var illness: Bool
+    var discipline: Float
+    var alive: Bool
+    var attention: Bool
+    var pooOnScreen: Bool
+    var lights: Bool
+    var sleeping: Bool
+    
+    
+    init(name: String) {
+        weight = 5
+        hungerHearts = 4.0
+        happyHearts = 4.0
+        self.name = name
+        age = 0
+        stage = "Baby"
+        illness = false
+        discipline = 0.0
+        alive = true
+        attention = false
+        pooOnScreen = false
+        lights = true
+        sleeping = false
+    }
+    
+    func eatSnack() {
+        hungerHearts += 1
+        weight += 2
+    }
+    func poo() {
+        pooOnScreen = true
+        weight -= 2
+    }
+}
--- TamagotchiConsole/main.swift
+++ TamagotchiConsole/main.swift
@@ -7,5 +7,13 @@
 
 import Foundation
 
+
 print("Hello, World!")
 
+let peteTamagotchi = Tamagotchi(name: "Pete")
+
+print(peteTamagotchi.happiness)
+peteTamagotchi.happiness -= 1
+print(peteTamagotchi.happiness)
+peteTamagotchi.eatSnack()
+print(peteTamagotchi.weight)
